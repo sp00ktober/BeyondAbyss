a123c79e5077408f8a367bdf40b7faba85fd1bb6
sp00ktober <ezra.knobloch@protonmail.com>
2023-05-02 12:14:17 +0200

    better day/night duration modification, sleep does reduced recover of sanity.

---
+++ BeyondAbyss/Patches/Dynamic/PlayerSanity_Patch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Winch.Core;
+
+namespace BeyondAbyss.Patches.Dynamic
+{
+    [HarmonyPatch(typeof(PlayerSanity))]
+    internal class PlayerSanity_Patch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PlayerSanity), "Update")]
+        public static void Update_Prefix()
+        {
+            // 2 is default
+            AccessTools.Field(typeof(GameConfigData), "sleepingSanityModifier").SetValue(GameManager.Instance.GameConfigData, 0.2f);
+        }
+    }
+}
---
+++ BeyondAbyss/Patches/Transpiler/TimeController_Transpiler.cs
@@ -0,0 +1,65 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using Winch.Core;
+
+namespace BeyondAbyss.Patches.Transpiler
+{
+    [HarmonyPatch(typeof(TimeController))]
+    internal class TimeController_Transpiler
+    {
+        [HarmonyTranspiler]
+        [HarmonyPatch(nameof(TimeController.GetTimePassageModifier))]
+        public static IEnumerable<CodeInstruction> GetTimePassageModifier_Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            CodeMatcher matcher = new CodeMatcher(instructions)
+                .MatchStartForward(
+                    new CodeMatch(OpCodes.Ldsfld),
+                    new CodeMatch(i => i.opcode == OpCodes.Callvirt && ((MethodInfo)i.operand).Name == "get_GameConfigData"),
+                    new CodeMatch(i => i.opcode == OpCodes.Callvirt && ((MethodInfo)i.operand).Name == "get_FishingTimePassageSpeedModifier"),
+                    new CodeMatch(OpCodes.Ret));
+
+            if (matcher.IsInvalid)
+            {
+                WinchCore.Log.Error("Could not match IL code in GetTimePassageModifier, not patching 1/2.");
+                return instructions;
+            }
+
+            matcher
+                .SetAndAdvance(OpCodes.Nop, null)
+                .SetAndAdvance(OpCodes.Nop, null)
+                .SetAndAdvance(OpCodes.Ldarg, 0)
+                .Insert(Helper.Transpiler.EmitDelegate<Func<TimeController, float>>((instance) =>
+                {
+
+                    return GameManager.Instance.GameConfigData.FishingTimePassageSpeedModifier * (instance.IsDaytime ? 1.5f : 0.5f);
+                }));
+
+            matcher
+                .MatchEndForward(
+                    new CodeMatch(OpCodes.Ldsfld),
+                    new CodeMatch(i => i.opcode == OpCodes.Callvirt && ((MethodInfo)i.operand).Name == "get_Player"),
+                    new CodeMatch(i => i.opcode == OpCodes.Callvirt && ((MethodInfo)i.operand).Name == "get_Controller"),
+                    new CodeMatch(i => i.opcode == OpCodes.Callvirt && ((MethodInfo)i.operand).Name == "get_InputMagnitude"),
+                    new CodeMatch(OpCodes.Ret));
+
+            if (matcher.IsInvalid)
+            {
+                WinchCore.Log.Error("Could not match IL code in GetTimePassageModifier, not patching 2/2.");
+                return instructions;
+            }
+
+            matcher
+                .InsertAndAdvance(new CodeInstruction(OpCodes.Ldarg_0))
+                .Insert(Helper.Transpiler.EmitDelegate<Func<float, TimeController, float>>((magnitude, instance) =>
+                {
+                    return magnitude * (instance.IsDaytime ? 1.5f : 0.5f);
+                }));
+
+            return matcher.InstructionEnumeration();
+        }
+    }
+}
